f804fdef34c81f9d6101cfb898c0262ffb9d397d
Linus41 <schief.craft@gmail.com>
2020-04-30 18:35:33 -0700

    Got update role function working

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ function createEmployee() {
             function (errOne, resOne) {
                 console.log("Your new employee has been added", resOne);
                 if (errOne) throw errOne;
-                updateRole();
+                assignRole();
             }
 
         );
@@ -131,181 +131,236 @@ function createRole() {
         });
     })
 
-    }
+}
+//doesn't work yet--assigns role to createEmployee
+function assignRole() {
+    connection.query("SELECT title FROM role", function (errOne, resOne) {
+        if (errOne) throw errOne;
+        console.log("resOne result", resOne);
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "employee",
+                message: "What role would you like to assign this employee?",
+                choices: resOne.map(role => {
+                    return {
+                        name: role.title,
+                        value: role.id
+                    }
+                })
 
-    function updateRole() {
-        connection.query("SELECT title FROM role", function (errOne, resOne) {
-            if (errOne) throw errOne;
+            }]).then(updatePrompt => {
+                connection.query(
+                    "UPDATE role SET ? WHERE ?", role.title, employee.role_id,
+                    [
+                        {
+                            role: updatePrompt.title
+                        }
+                    ],
+                    function (errTwo, resTwo) {
+                        if (errTwo) throw errTwo;
+                        trackerChoices();
+                    }
+                );
+            });
+
+    });
+
+}
+// updates employee's role
+function updateRole() {
+    // locates an employee ideally by first and last name, but it's only returning last so far
+    connection.query("SELECT first_name, last_name, id FROM employee", function (errOne, resOne) {
+        connection.query("SELECT * FROM role", function (errTwo, resTwo) {
             console.log("resOne result", resOne);
+            if (errOne) throw errOne;
             inquirer.prompt([
                 {
                     type: "list",
                     name: "employee",
-                    message: "What role would you like to assign this employee?",
-                    choices: resOne.map(role => {
+                    message: "Which employee would you like to update?",
+                    // displays list of all employees
+                    choices: resOne.map(employee => {
                         return {
-                            name: role.title,
-                            value: role.id
+                            name: employee.first_name + " " + employee.last_name,
+                            value: employee.id
+
                         }
                     })
 
+                },
+                {
+                    type: "list",
+                    name: "newRole",
+                    message: "What role would you like to assign this employee?",
+                    choices:  resTwo.map(role => {
+                        return {
+                            name: role.title,
+                            value: role.id
+
+                        }
+                    })
+                    // updatePrompt is object that holds all of the answer values for inquirer q's
                 }]).then(updatePrompt => {
                     connection.query(
-                        "UPDATE role SET ? WHERE ?", role.title, employee.role_id,
+                        // updates 
+                        "UPDATE employee SET role_id = ? WHERE id = ?",
                         [
-                            {
-                                role: updatePrompt.title
-                            }
+                            updatePrompt.newRole,
+                            updatePrompt.employee
                         ],
-                        function (errTwo, resTwo) {
-                            if (errTwo) throw errTwo;
+                        function (updateErr, update) {
+
+                            if (updateErr) throw updateErr;
                             trackerChoices();
                         }
                     );
                 });
 
         });
+    })
 
-    }
+
+}
 
 
-    function deleteEmployee() {
-        connection.query("SELECT * FROM products", function (errOne, resOne) {
-            if (errOne) throw errOne;
-            inquirer.prompt([
-                {
-                    type: "list",
-                    name: "id",
-                    message: "What Flavor Id do you want to delete?",
-                    choices: resOne.map(product => {
-                        return {
-                            name: product.flavor,
-                            value: product.id
-                        }
-                    })
-                }]).then(deletePrompt => {
-                    connection.query(
-                        "DELETE FROM products WHERE ?",
-                        {
-                            id: deletePrompt.id
-                        },
-                        function (errTwo, resTwo) {
-                            if (errTwo) throw errTwo;
-                            iceCreamManager();
-                        }
-                    );
-                });
-        });
-    }
+function deleteEmployee() {
+    connection.query("SELECT * FROM products", function (errOne, resOne) {
+        if (errOne) throw errOne;
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "id",
+                message: "What Flavor Id do you want to delete?",
+                choices: resOne.map(product => {
+                    return {
+                        name: product.flavor,
+                        value: product.id
+                    }
+                })
+            }]).then(deletePrompt => {
+                connection.query(
+                    "DELETE FROM products WHERE ?",
+                    {
+                        id: deletePrompt.id
+                    },
+                    function (errTwo, resTwo) {
+                        if (errTwo) throw errTwo;
+                        iceCreamManager();
+                    }
+                );
+            });
+    });
+}
 
-    // display all employees
-    function readEmployees() {
-        connection.query("SELECT * FROM employee", function (err, res) {
-            console.table(res)
-            if (err) throw err;
-            trackerChoices();
-        });
-    }
-    //   display all departments
-    function readDepartments() {
-        connection.query("SELECT * FROM department", function (err, res) {
-            console.table(res)
-            if (err) throw err;
-            trackerChoices();
-        });
-    }
-    //   display all roles
-    function readRoles() {
-        connection.query("SELECT * FROM role", function (err, res) {
-            console.table(res)
-            if (err) throw err;
-            trackerChoices();
-        });
-    }
-    // display table with all info
-    // do I use joins here to create a table that shows everything?
-    function viewTable() {
-        connection.query("SELECT * FROM employee INNER JOIN role ON employee.role_id = role.id", function (err, res) {
-            console.table(res)
-            if (err) throw err;
-            trackerChoices();
-        });
-    }
+// display all employees
+function readEmployees() {
+    connection.query("SELECT * FROM employee", function (err, res) {
+        console.table(res)
+        if (err) throw err;
+        trackerChoices();
+    });
+}
+//   display all departments
+function readDepartments() {
+    connection.query("SELECT * FROM department", function (err, res) {
+        console.table(res)
+        if (err) throw err;
+        trackerChoices();
+    });
+}
+//   display all roles
+function readRoles() {
+    connection.query("SELECT * FROM role", function (err, res) {
+        console.table(res)
+        if (err) throw err;
+        trackerChoices();
+    });
+}
+// display table with all info
+// do I use joins here to create a table that shows everything?
+function viewTable() {
+    connection.query("SELECT first_name, last_name, employee.id, role.title, role.salary, department.name FROM employee INNER JOIN role ON role_id = role.id INNER JOIN department ON role.department_id = department.id", function (err, res) {
+        console.table(res)
+        if (err) throw err;
+        trackerChoices();
+    });
+}
 
-    //   asks user at beginning what they'd like to see: employees, depts, roles
-    function trackerChoices() {
-        inquirer.prompt([{
-            message: "What would you like to do?",
-            name: "choice",
-            type: "list",
-            choices: [
-                {
-                    name: "Add a department",
-                    value: "addDepartment"
-                },
-                {
-                    name: "Add a role",
-                    value: "addRole"
-                },
-                {
-                    name: "Add an Employee",
-                    value: "addEmployee"
-                },
-                {
-                    name: "See all employees",
-                    value: "readEmployees"
-                },
+//   asks user at beginning what they'd like to see: employees, depts, roles
+function trackerChoices() {
+    inquirer.prompt([{
+        message: "What would you like to do?",
+        name: "choice",
+        type: "list",
+        choices: [
+            {
+                name: "Add a department",
+                value: "addDepartment"
+            },
+            {
+                name: "Add a role",
+                value: "addRole"
+            },
+            {
+                name: "Add an Employee",
+                value: "addEmployee"
+            },
+            {
+                name: "See all employees",
+                value: "readEmployees"
+            },
 
-                {
-                    name: "See all departments",
-                    value: "readDepartments"
-                },
+            {
+                name: "See all departments",
+                value: "readDepartments"
+            },
 
-                {
-                    name: "See all roles",
-                    value: "readRoles"
-                },
+            {
+                name: "See all roles",
+                value: "readRoles"
+            },
 
-                {
-                    name: "Update employee a role",
-                    value: "updateRole"
-                },
-                {
-                    name: "View table",
-                    value: "everything",
-                },
-                {
-                    name: "Quit",
-                    value: "quit"
-                }
-            ]
-            //   sends user to appropriate functions
-        }]).then(actionPrompt => {
-            // console.log(actionPrompt.choice)
-            if (actionPrompt.choice === "readEmployees") {
-                readEmployees();
-            } else if (actionPrompt.choice === "addEmployee") {
-                createEmployee();
-            } else if (actionPrompt.choice === "readDepartments") {
-                readDepartments();
-            } else if (actionPrompt.choice === "addDepartment") {
-                createDepartment();
-            } else if (actionPrompt.choice === "readRoles") {
-                readRoles();
-            } else if (actionPrompt.choice === "addRole") {
-                createRole();
-            } else if (actionPrompt.choice === "everything") {
-                viewTable();
-            } else if (actionPrompt.choice === "updateRole") {
-                updateRole();
-            } else {
-                console.log("goodbye");
-                process.exit()
-                return;
+            {
+                name: "Update employee a role",
+                value: "updateRole"
+            },
+            {
+                name: "View table",
+                value: "everything",
+            },
+            {
+                name: "Quit",
+                value: "quit"
             }
-        })
-        // return;
-        // console.table();
-    }
+        ]
+        //   sends user to appropriate functions
+    }]).then(actionPrompt => {
+        // console.log(actionPrompt.choice)
+        if (actionPrompt.choice === "readEmployees") {
+            readEmployees();
+        } else if (actionPrompt.choice === "addEmployee") {
+            createEmployee();
+        } else if (actionPrompt.choice === "readDepartments") {
+            readDepartments();
+        } else if (actionPrompt.choice === "addDepartment") {
+            createDepartment();
+        } else if (actionPrompt.choice === "readRoles") {
+            readRoles();
+        } else if (actionPrompt.choice === "addRole") {
+            createRole();
+        } else if (actionPrompt.choice === "everything") {
+            viewTable();
+        } else if (actionPrompt.choice === "updateRole") {
+            updateRole();
+        } else {
+            console.log("goodbye");
+            process.exit()
+            return;
+        }
+    })
+    // return;
+    // console.table();
+}
 
 
